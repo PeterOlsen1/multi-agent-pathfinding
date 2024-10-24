1613b08fcff9e9ea0664ac5e3083e21311b7b4f5
Peter Olsen <olse0321@umn.edu>
2024-10-23 19:28:53 -0500

    Worked out a* search

--- agent.py
+++ agent.py
@@ -5,6 +5,79 @@ class Agent():
         self.j = j
         self.goal_i = goal_i
         self.goal_j = goal_j
+        self.frontier = []
+
+
+    def is_goal(self):
+        '''
+        Return True if the agent is in its goal state
+        '''
+        return self.i == self.goal_i and self.j == self.goal_j
+    
+
+    def heuristic(self, i=None, j=None):
+        '''
+        Give the straightline distance between current position and goal, ignoring obstacles
+        '''
+        if not i:
+            i = self.i
+        if not j:
+            j = self.j
+        return ((self.goal_i - i) ** 2 + (self.goal_j - j) ** 2) ** (1/2)
+        
+
+    def open_moves(self, board):
+        '''
+        Returns a list of open moves for the given board
+        '''
+        options = [
+            (self.i, self.j),
+            (self.i + 1, self.j),
+            (self.i + 1, self.j + 1),
+            (self.i + 1, self.j - 1),
+            (self.i, self.j + 1),
+            (self.i, self.j - 1),
+            (self.i - 1, self.j + 1),
+            (self.i - 1, self.j),
+            (self.i - 1, self.j - 1),
+        ]
+
+        out = []
+        n = len(board)
+        for coord in options:
+            i, j = coord
+            is_valid = 0 <= i < n and 0 <= j < n
+            if is_valid and not board[i][j]:
+                out.append(coord)
+        return out
+    
+
+    def move(self, board):
+        if self.is_goal():
+            return
+        
+        moves = self.open_moves(board)
+        self.frontier += moves
+        self.sort_frontier()
+        if ((self.goal_i, self.goal_j) in self.frontier):
+            self.i, self.j = self.goal_i, self.goal_j
+            return
+        coord = self.frontier.pop(0)
+
+        board[self.i][self.j] = 0
+        self.i, self.j = coord
+        board[self.i][self.j] = self
+
+
+
+    def sort_frontier(self):
+        self.frontier.sort(key=lambda coord: self.heuristic(coord[0], coord[1]) + 1)
 
     def __repr__(self):
         return f'Agent at position ({self.i}, {self.j}) color {self.color}'
+    
+
+
+if __name__ == '__main__':
+    test = Agent((200, 200, 200), 0, 0, 10, 10)
+    print(test.heuristic())
--- board.py
+++ board.py
@@ -1,16 +1,17 @@
 import pygame
 import random
 from agent import Agent
+import time
 
 
 # Initialize pygame
 pygame.init()
 
 # Screen dimensions
-width, height = 800, 800
+width, height = 700, 700
 screen = pygame.display.set_mode((width, height))
 
-rows, cols = 20, 20
+rows, cols = 30, 30
 cell_width = width // cols
 cell_height = height // rows
 
@@ -44,7 +45,7 @@ def generate_board(num_islands, min_island_size, max_island_size):
             choice_in_range = 0 < choice_i < (cols - 1) and 0 < choice_j < (cols - 1)
             tries = 0
 
-            while tries < 30 and choice_in_range and board[choice_i][choice_j] == 'block':
+            while tries < 30 and choice_in_range and board[choice_i][choice_j]:
                 choice_i, choice_j = random.choice(choice_list)
                 choice_in_range = 0 < choice_i < (cols - 1) and 0 < choice_j < (cols - 1)
                 tries += 1
@@ -53,6 +54,13 @@ def generate_board(num_islands, min_island_size, max_island_size):
     return board   
 
 def place_agents(num_agents, board):
+    '''
+    Create {num_agents} agents and place them on the given board
+
+    Parameters:
+        num_agents (int): the number of agents to place on the board
+        board ((Agent | int | String) [][]) the given board
+    '''
     for _ in range(num_agents):
         tries = 0
         i, j = random.randint(0, rows - 1), random.randint(0, cols - 1)
@@ -62,51 +70,67 @@ def place_agents(num_agents, board):
 
         tries = 0
         goal_i, goal_j = random.randint(0, rows - 1), random.randint(0, cols - 1)
-        while tries < 30 and board[i][j]:
+        while board[i][j]:
             goal_i, goal_j = random.randint(0, rows - 1), random.randint(0, cols - 1)
-            tries += 1
 
         agent = Agent(make_random_color(), i, j, goal_i, goal_j)
         agents.append(agent)
         board[i][j] = agent
+        board[goal_i][goal_j] = 1
 
 
 def draw_board(board):
     '''
-    Draws the game board on the screen
+    Draws the game board on the screen.
+
+    The default square is an open white box,
+    the default block is a grey block,
+    and agens are rendered in two parts:
+        Agent is a circle, and goal is a ring
     '''
     for row in range(rows):
         for col in range(cols):
             rect = pygame.Rect(col * cell_width, row * cell_height, cell_width, cell_height)
 
-            if isinstance(board[row][col], Agent):
-                agent = board[row][col]
-                # draw the agent's starting position
-                center = (col * cell_width + cell_width // 2, row * cell_height + cell_height // 2)
-                radius = cell_height // 2 - 5
-                pygame.draw.circle(screen, agent.color, center, radius)
-
-                # draw the agent's goal position
-                center = (agent.goal_i * cell_width + cell_width // 2, agent.goal_j * cell_height + cell_height // 2)
-                pygame.draw.circle(screen, agent.color, center, radius)
-                pygame.draw.circle(screen, WHITE, center, radius - 2)
-
-            elif (board[row][col] ==  'block'):
+            if (board[row][col] ==  'block'):
                 pygame.draw.rect(screen, GREY, rect)
 
             else:
                 pygame.draw.rect(screen, WHITE, rect)
             pygame.draw.rect(screen, BLACK, rect, 1)
-    
+    for agent in agents:
+        # draw the agent's starting position
+        center = (agent.j * cell_width + cell_width // 2, agent.i * cell_height + cell_height // 2)
+        radius = cell_height // 2 - 5
+        pygame.draw.circle(screen, agent.color, center, radius)
+
+        # draw the agent's goal position
+        center = (agent.goal_j * cell_width + cell_width // 2, agent.goal_i * cell_height + cell_height // 2)
+        pygame.draw.circle(screen, agent.color, center, radius)
+        pygame.draw.circle(screen, WHITE, center, radius - 3)
+
     # this function call updates the entire state of the pygame window
     pygame.display.update()
 
 
-board = generate_board(10, 5, 10)
-place_agents(10, board)
+board = generate_board(10, 10, 12)
+place_agents(4, board)
+
+
+screen.fill(WHITE)
+draw_board(board)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == 1025 or event.type == 1026:
+            pygame.quit()
+    for agent in agents:
+        agent.move(board)
+    time.sleep(0.1)
+
+
     screen.fill(WHITE)
     draw_board(board)
+    
